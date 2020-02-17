f75824b5e01d2a95854cc3e26dacf98e26de4d8c
HFYEH <sharefun010407@gmail.com>
2020-02-17 12:14:40 +0800

    Temp commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+       <highcharts :options="chartOptions"></highcharts>
+       <highcharts :constructor-type="'stockChart'" :options="chartOptions"></highcharts>
   </div>
 </template>
 
 <script>
+
+    import Highcharts from 'highcharts'
+    import exportingInit from 'highcharts/modules/exporting'
+    import stockInit from 'highcharts/modules/stock'
+    import {Chart} from 'highcharts-vue'
+
+    stockInit(Highcharts)
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue xxx!",
+      chartOptions: {
+        chart: {
+          type: 'spline'
+        },
+        title: {
+          text: 'Entire title'
+        },
+        series: [{
+          data: [10, 0, 8, 2, 6, 4, 5, 5]
+        }]
+      },
+
     }
-  }
+  },
+      components: {
+        highcharts: Chart
+      }
+
 }
 </script>
 
